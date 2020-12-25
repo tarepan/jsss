b873ac8a7779cb9eba63010acf8a996c4367d8b8
Tarepan <tarepan5884@gmail.com>
2020-12-25 02:56:14 +0000

    Refactor duplicated utility

--- jsss/utils.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-def hash_args(*args) -> str:
-    contents = ""
-    for c in args:
-        contents = f"{contents}_{str(c)}"
-    contents = hashlib.md5(contents.encode('utf-8')).hexdigest()
-    return contents
